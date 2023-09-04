de2f48ec796ba9e6aaa9a137a1c36a328c00ef65
Joe O'Connor <joeoc2625@gmail.com>
2023-09-04 14:22:49 +0100

    Add padded methods for allocation and manipulation of padded buffers

--- src/lib.rs
+++ src/lib.rs
@@ -13,9 +13,26 @@ pub use game::Game;
 pub use game::GameCommand;
 pub use game::GameData;
 pub use game::GameInitialisationFailure;
+use wgpu::util::DeviceExt;
 pub mod input {
     pub use crate::game::input::*;
 }
+
+fn next_multiple_of(
+    value: wgpu::BufferAddress,
+    multiple: wgpu::BufferAddress,
+) -> wgpu::BufferAddress {
+    match value % multiple {
+        0 => value,
+        r => value + (multiple - r),
+    }
+}
+
+/// Some operations care about alignment in such a way that it is often easier to simply round all buffer sizes up to the nearest
+/// multiple of some power of two. This constant gives that power of two, and the corresponding [`LfDeviceExt::create_buffer_padded`],
+/// [`LfDeviceExt::new_buffer_init_padded`] and [`LfQueueExt::write_buffer_padded`] all extend their data lengths to the nearest
+/// multiple of this constant.
+pub const BUFFER_PADDING: wgpu::BufferAddress = 256;
 
 // Link in to existing objects
 // We're only adding methods to specific wgpu objects
@@ -35,6 +52,9 @@ mod sealed {
     pub trait SealedBuffer {}
     impl SealedBuffer for wgpu::Buffer {}
 
+    pub trait SealedQueue {}
+    impl SealedQueue for wgpu::Queue {}
+
     pub trait SealedBindGroupLayoutEntry {}
     impl SealedBindGroupLayoutEntry for wgpu::BindGroupLayoutEntry {}
 
@@ -43,7 +63,20 @@ mod sealed {
     impl<T: crate::Game> SealedGame for T {}
 }
 
+pub struct PaddedBufferInitDescriptor<'a> {
+    /// Debug label of a buffer. This will show up in graphics debuggers for easy identification.
+    pub label: wgpu::Label<'a>,
+    /// Contents of a buffer on creation. Will be extended to the next pad interval.
+    pub contents: Vec<u8>,
+    /// Usages of a buffer. If the buffer is used in any way that isn't specified here, the operation
+    /// will panic.
+    pub usage: wgpu::BufferUsages,
+}
+
 pub trait LfDeviceExt: sealed::SealedDevice {
+    fn create_buffer_padded(&self, desc: wgpu::BufferDescriptor) -> wgpu::Buffer;
+    fn create_buffer_init_padded(&self, desc: PaddedBufferInitDescriptor) -> wgpu::Buffer;
+
     fn create_fragment_only_render_bundle_encoder(
         &self,
         desc: &FragmentOnlyRenderBundleEncoderDescriptor,
@@ -56,6 +89,23 @@ pub trait LfDeviceExt: sealed::SealedDevice {
 }
 
 impl LfDeviceExt for wgpu::Device {
+    fn create_buffer_padded(&self, mut desc: wgpu::BufferDescriptor) -> wgpu::Buffer {
+        desc.size = next_multiple_of(desc.size, BUFFER_PADDING);
+
+        self.create_buffer(&desc)
+    }
+
+    fn create_buffer_init_padded(&self, mut desc: PaddedBufferInitDescriptor) -> wgpu::Buffer {
+        let new_len = next_multiple_of(desc.contents.len() as wgpu::BufferAddress, BUFFER_PADDING);
+        desc.contents.resize(new_len as usize, 0u8);
+
+        self.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: desc.label,
+            contents: &desc.contents,
+            usage: desc.usage,
+        })
+    }
+
     fn create_fragment_only_render_bundle_encoder(
         &self,
         desc: &FragmentOnlyRenderBundleEncoderDescriptor,
@@ -102,6 +152,40 @@ impl LfLimitsExt for wgpu::Limits {
     /// Gets the set of limits supported by either this ot the other limits.
     fn union<'a>(&self, other: &wgpu::Limits) -> wgpu::Limits {
         crate::limits::limits_union(self, other)
+    }
+}
+
+pub trait LfQueueExt: sealed::SealedQueue {
+    /// Writes the given data to the given buffer using [`wgpu::Queue::write_buffer`],
+    /// but pads the data to the nearest multiple of the alignment required for buffer writing.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the padded data would overrun the given buffer.
+    fn write_buffer_padded(
+        &self,
+        buffer: &wgpu::Buffer,
+        offset: wgpu::BufferAddress,
+        data: Vec<u8>,
+    );
+}
+
+impl LfQueueExt for wgpu::Queue {
+    fn write_buffer_padded(
+        &self,
+        buffer: &wgpu::Buffer,
+        offset: wgpu::BufferAddress,
+        mut data: Vec<u8>,
+    ) {
+        const PAD_ALIGNMENT: usize = BUFFER_PADDING as usize;
+        let len = data.len();
+        let target_size = match len % PAD_ALIGNMENT {
+            0 => len,
+            r => len + (PAD_ALIGNMENT - r),
+        };
+        data.resize(target_size, 0u8);
+
+        self.write_buffer(buffer, offset, &data)
     }
 }
 
